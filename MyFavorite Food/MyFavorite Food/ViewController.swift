aed112c51302ac91a9e10afa4a23eccd686c2d48
竺培豪 <zhupeihao@zhupeihaodeMacBook-Pro.local>
2022-09-18 16:38:04 -0700

    Adds-ZhiweiLin--assignment2

---
+++ MyFavorite Food/MyFavorite Food/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MyFavorite Food
+//
+//  Created by 竺培豪 on 9/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    @IBOutlet weak var tblView: UITableView!
+    
+    let foods = ["Pizza", "Chicken", "Noodles", "Grapes", "Eggplant", "Egg", "Apple", "Cake"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tblView.delegate=self
+        tblView.dataSource=self
+
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foods.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell",for: indexPath)
+        cell.textLabel?.text=foods[indexPath.row]
+        return cell
+    }
+    
+
+
+}
+
